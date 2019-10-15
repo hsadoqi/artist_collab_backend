c0f6187d319507637e8de75d4b16f1e89dd68549
Hanaa Sadoqi <hsadoqi@gmail.com>
2019-10-15 15:18:29 -0400

    user and routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
 
-    def new 
-        @user = User.new
+    def show 
+        set_user
     end 
 
     def create 
         @user = User.new(user_params)
 
         if @user.save 
-            render :json, { user: @user, message: "Success" }
+            render json: { user: @user, message: "Success" }
         else 
             render json: { errors: @user.errors.full_messages }, status: 422
         end 
@@ -16,11 +16,17 @@ class UsersController < ApplicationController
 
     private 
 
+    def set_user 
+        @user = User.find_by_id(params[:id])
+        render json: { user: @user )
+    end 
+
     def user_params 
-        params.permit(
+        params.require(:user).permit(
             :name, 
             :email, 
             :username, 
             :password)
     end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get '/signup', to: 'users#new'
-  post '/users', to: 'users#create'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
